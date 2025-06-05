0cca3ce68ce01ae90fd17dfa88a22f1ae3dd493e
ffreshaf <up202307205@up.pt>
2025-06-05 23:50:30 +0100

    spinning and spinning

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 
 function App() {
+  const [spinning, setSpinning] = useState(false);
+
+  const handleLogoClick = () => {
+    setSpinning(true);
+    setTimeout(() => setSpinning(false), 2000);
+  }
 
   return (
     <>
       <header>
         <h1>Welcome to the By Flora project</h1>
-        <img src="/By-Flora.png" alt="By Flora Logo" className="logo" />
+        <img src="/By-Flora.png" alt="By Flora Logo" className={`logo ${spinning ? 'spinning' : ''}`} onClick={handleLogoClick} />
         <nav>
           <Link to="/" style={{ marginRight: '10px'}}>Home</Link>
           <Link to="/about">About</Link>
