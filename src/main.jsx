ccd2978933f877ff02ac075759fa98428880e109
Guilherme Oliveira Linhares <up202307295@up.pt>
2026-09-06 18:10:36 +0100

    sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/capacitor'
+import * as SentryReact from '@sentry/react'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import App from './App.jsx'
+
+Sentry.init(
+  {
+    dsn: 'https://6d3a416fd99e2f0dfa0f1fde9d3f9d10@o4512040452751360.ingest.de.sentry.io/4512040478376016',
+  },
+  SentryReact.init
+);
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
